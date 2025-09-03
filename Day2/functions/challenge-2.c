762cb970f352ac2571710e1bae0a24c218f02e18
sahhoutiamine <midoriaduko@gmail.com>
2025-09-03 10:23:43 +0100

    fix the function challenge N2

--- Day2/functions/challenge-2.c
+++ Day2/functions/challenge-2.c
@@ -1,23 +1,55 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// void reverseChaine (char str[]){
+//     int strLenght = strlen(str);
+//     char reverseTable [100];
+//     for (int i = strLenght; i >= 0; i--) {
+//         for (int j = 0; j <= strLenght; j++) {
+//             reverseTable[j] = str[i];
+//         }
+//     }
+//     for (int j = 0; j <= strLenght; j++) {
+//             printf("%s", reverseTable[j]);
+//         }
+// }
+
+// void main () {
+//     char str[100];
+//     printf("entrer une chaine :");
+//     scanf("%s", &str);
+//     reverseChaine(str);
+// }
 #include <stdio.h>
+#include<stdlib.h>
 #include <string.h>
 
-void reverseChaine (char str[]){
-    int strLenght = strlen(str);
-    char reverseTable [100];
-    for (int i = strLenght; i >= 0; i--) {
-        for (int j = 0; j <= strLenght; j++) {
-            reverseTable[j] = str[i];
-        }
+char* reverseChaine(char *str,int strLength) {
+    //int strLength = strlen(str);
+    char* reverseTable;
+    reverseTable = (char*)malloc((strLength+1) * sizeof(char));
+    for (int i = 0; i<strLength; i++) {
+        reverseTable[i] = str[strLength - i - 1];
+        reverseTable[strLength] = '\0';
     }
-    for (int j = 0; j <= strLenght; j++) {
-            printf("%s", reverseTable[j]);
-        }
+    return reverseTable;
 }
-
 void main () {
-    char str[100];
+    //char* str;
+    char str[20];
+    int len = strlen(str);
+    char * revResult ;//= (char*)malloc(len * sizeof(char));;
     printf("entrer une chaine :");
-    scanf("%s", &str);
-    reverseChaine(str);
-}
+    // strcpy(str,str2);
+    //scanf("%20s", &str);
+    fgets(str, sizeof(str), stdin);
+    //gets(str);
+    /*if (fgets(str, sizeof(str), stdin)) {
+        // Remove newline character if present
+        str[strcspn(str, "\n")] = '\0';
+    }*/
+    revResult = reverseChaine(str,len);
+    printf("%20s", str);
+    printf("%20s", revResult);
 
+}
