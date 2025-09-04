aa9355efcef7271f2191391b542a78f7ee960809
sahhoutiamine <midoriaduko@gmail.com>
2025-09-04 10:31:42 +0100

    fix mini-projet error

--- Day2/mini-projet.c
+++ Day2/mini-projet.c
@@ -3,44 +3,134 @@
 
 
 
-char* livreCreate (char nomLivre[20], char auteur[20], char prix[6], char qteStock[10]) {
+//char* livreCreate (char nomLivre[20], char auteur[20], char prix[10], char qteStock[10]) {
 
-    char livre[100];
+   
+
+
+
     
-    strcat(livre, nomLivre);
-    strcat(livre, "\n");
-    strcat(livre, auteur);
-    strcat(livre, "\n");
-    strcat(livre, prix);
-    strcat(livre, "\n");
-    strcat(livre, qteStock);
-    strcat(livre, "\n");
+    // strcat(livre, nomLivre);
+    // strcat(livre, "\n");
+    // strcat(livre, auteur);
+    // strcat(livre, "\n");
+    // strcat(livre, prix);
+    // strcat(livre, "\n");
+    // strcat(livre, qteStock);
+    // strcat(livre, "\n");
+    
+    // char* res = livre;
 
-    return livre ; 
-}
+    // return res ; 
+    // printf("%s", res);
+// }
 
 
 void main () {
-    char* livresTable [100];
+    // char* livresTable [100];
+    // int nmbrLivre;
+    // char* livre;
+    // char nomLivre[20];
+    // char auteur[20];
+    // char prix[10];
+    // char qteStock [10];
+    // printf("Titre de livre :");
+    // scanf("%s", &nomLivre);
+    // printf("Auteur de livre :");
+    // scanf("%s", &auteur);
+    // printf("Prix de livre :");
+    // scanf("%s", &prix);
+    // printf("Qte de livre :");
+    // scanf("%s", &qteStock);
+    // // livreCreate(nomLivre, auteur, prix, qteStock);
+    // livre = livreCreate(nomLivre, auteur, prix, qteStock);
+    // printf("%s", livre);
+
+    // printf("entrer le nomre du livres :");
+    // scanf("%d", &nmbrLivre);
+    // for (int i = 0 ; i < nmbrLivre ; i++) {
+    //     printf("Titre de livre :");
+    //     scanf(" %20s", &nomLivre);
+    //     printf("Auteur de livre :");
+    //     scanf(" %20s", &auteur);
+    //     printf("Prix de livre :");
+    //     scanf("%d", &prix);
+    //     printf("Qte de livre :");
+    //     scanf("%d", &qteStock);
+    //     livre = livreCreate(nomLivre, auteur, prix, qteStock);
+    //     livresTable[i] = livre ;
+    //     printf("%100s", livre)
+    // }
+
+
+
+    char nomlivres[100][20];
+    char auteurslivres[100][20];
+    int prixlivres[100];
+    int qtelivres[100];
     int nmbrLivre;
-    char* livre;
+
+    printf("entrer le nombre du livres :");
+    scanf("%d", &nmbrLivre);
+
     char nomLivre[20];
     char auteur[20];
     int prix;
     int qteStock;
-    printf("entrer le nomre du livres :");
-    scanf("%d", &nmbrLivre);
-    for (int i = 0 ; i < nmbrLivre ; i++) {
+
+    for (int i = 0  ;  i < nmbrLivre; i++) 
+    {
         printf("Titre de livre :");
-        scanf(" %20s", &nomLivre);
+        scanf("%s", &nomLivre);
         printf("Auteur de livre :");
-        scanf(" %20s", &auteur);
+        scanf("%s", &auteur);
         printf("Prix de livre :");
         scanf("%d", &prix);
         printf("Qte de livre :");
         scanf("%d", &qteStock);
-        livre = livreCreate(nmbrLivre, auteur, prix, qteStock);
-        livresTable[i] = livre ;
+
+        strcpy(nomlivres[i], nomLivre);
+        strcpy(auteurslivres[i], auteur);
+        prixlivres[i] = prix;
+        qtelivres[i] = qteStock;
+        
+
     }
+
+    for  (int i = 0  ;  i < nmbrLivre; i++) {
+
+            printf("Livre %d :\n%s\n", i+1, nomlivres[i]);
+            printf("%s\n", auteurslivres[i]);
+            printf("%d\n", prixlivres[i]);
+            printf("%d\n\n", qtelivres[i]);
+            
+        }
+
+
+
+
+    // for (int i = 0  ;  i < nmbrLivre; i++) 
+    // {
+    //     printf("Titre de livre :");
+    //     scanf("%s", &nomLivre);
+    //     printf("Auteur de livre :");
+    //     scanf("%s", &auteur);
+    //     printf("Prix de livre :");
+    //     scanf("%d", &prix);
+    //     printf("Qte de livre :");
+    //     scanf("%d", &qteStock);
+    //     for (int j = 0 ; j < nmbrLivre ; j++) {
+    //             nomlivres[j][k] = nomLivre; 
+    //             auteurslivres[j][k] = auteur;  
+    //     }    
+        
+        
+
+    // }
+
+
+
+
+
 
 }
