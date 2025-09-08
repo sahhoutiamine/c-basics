678aafa619855e2b0887f4e0c9c44dca82a89ff7
sahhoutiamine <midoriaduko@gmail.com>
2025-09-08 18:56:53 +0100

    complete mini-projet_2

--- Day3/mini-projet_2.c
+++ Day3/mini-projet_2.c
@@ -5,9 +5,9 @@
 
 
 typedef struct Contact {
-    char nom[16];
-    char numTel[12];
-    char email[64];
+    char nom[MAX_LENGHT];
+    char numTel[MAX_LENGHT];
+    char email[MAX_LENGHT];
 } Contact;
 
 
@@ -31,21 +31,22 @@ void main ()
     switch (actNum)
     {
     case 1:
-        printf("\nEntrer le nom : ->");
         getchar();
+        printf("\nEntrer le nom : \n");
+        //getchar();
         fgets(contact[cmp].nom, sizeof(contact->nom), stdin);
-        printf("\nEntrer le numero de tel : ->");
-        getchar();
+        printf("Entrer le numero de tel : \n");
+        //getchar();
         fgets(contact[cmp].numTel, sizeof(contact->numTel), stdin);
-        printf("\nEntrer l'email : ->");
-        getchar();
+        printf("Entrer l'email : \n");
+        //getchar();
         fgets(contact[cmp].email, sizeof(contact->email), stdin);
         cmp++;
         break;
     case 2:
-        char nomContMod [16];
-        printf("\nEntrer le nom de contact : ->");
+        char nomContMod [MAX_LENGHT];
         getchar();
+        printf("\nEntrer le nom de contact :\n ->");
         fgets(nomContMod, sizeof(nomContMod), stdin);
         for (int i = 0 ; i < cmp ; i++) 
         {
@@ -63,20 +64,20 @@ void main ()
                     switch (infoMod)
                     {
                     case 1:
-                        printf("\nEntrer le noveau nom : ->");
                         getchar();
+                        printf("\nEntrer le noveau nom :\n ->");
                         fgets(contact[i].nom, sizeof(contact->nom), stdin);
                         break;
 
                     case 2:
-                        printf("\nEntrer le noveau numero de telephone : ->");
                         getchar();
+                        printf("\nEntrer le noveau numero de telephone :\n ->");
                         fgets(contact[i].numTel, sizeof(contact->numTel), stdin);
                         break;
                     
                     case 3:
-                        printf("\nEntrer le noveau email : ->");
                         getchar();
+                        printf("\nEntrer le noveau email :\n ->");
                         fgets(contact[i].email, sizeof(contact->email), stdin);
                         break;
                     
@@ -93,20 +94,99 @@ void main ()
         }
         break;
 
+
+
     case 3:
 
 
         //  printf("\nLe contact %d :\n%s%s%s", 1, contact[0].nom, contact[0].numTel, contact[0].email);
+        printf("\n");
         for (int i = 0 ; i<cmp ; i++) 
         {
-            printf("\nLe contact %d :\n%s%s%s", i+1, contact[i].nom, contact[i].numTel, contact[i].email);
+            printf("\nLe contact %d : \n\n%s\n%s\n%s\n", i+1, contact[i].nom, contact[i].numTel, contact[i].email);
 
         }
 
         break;
+    
+    case 4:
+        char nomContRech [MAX_LENGHT];
+        int isTrue = 1;
+        // int index ;
+        getchar();
+        printf("\nEntrer le nom de contact :\n ->");
+        fgets(nomContRech, sizeof(nomContRech), stdin);
+        for (int i = 0 ; i < cmp ; i++) 
+        {
+            if (strcmp(contact[i].nom, nomContRech) == 0) 
+            {
+               printf("\nLe contact est exist!\n\n");
+               isTrue = 1;
+               break;
+            
+            }
+            else
+            {  
+                isTrue = 0;
+            }
+            
+            
+        }
+        if (isTrue == 0)
+        {
+            printf("n'exist pas!\n\n");
+        }
         
+        // if (isTrue == 1)
+        // {
+        //     printf("\nLe contact est exist!\n\n%s\n%s\n%s\n", contact[index].nom, contact[index].numTel, contact[index].email);
+        // }    
+        // else
+        // {
+        //     printf("n'exist pas!");
+        // }
+        
+        
+        break;
+
+    case 5:
+        char nomContSupp [MAX_LENGHT];
+        int isDel = 1;
+        getchar();
+        printf("\nEntrer le nom de contact : ->");
+        fgets(nomContSupp, sizeof(nomContSupp), stdin);
+        for (int i = 0 ; i < cmp ; i++) 
+        {
+            if (strcmp(contact[i].nom, nomContRech) == 0) 
+            {
+                for (int j = i ; j < cmp ; j++)
+                {
+                    strcpy(contact[j].nom, contact[j+1].nom);
+                    strcpy(contact[j].numTel, contact[j+1].numTel);
+                    strcpy(contact[j].email, contact[j+1].email);
+                }
+                printf("\nSupprimer avec secces!\n");
+                isDel = 1;
+                cmp--;
+                break;
 
 
+            }
+            else
+            {
+                isDel = 0;
+                
+            }
+               
+        }
+        if (isDel == 0)
+        {
+            printf("\nn'exsit pas!");
+        }
+        
+        
+        
+        break;
 
     
     case 0:
